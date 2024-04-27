f27e9abb97e74b03b57d752f91ee1bfb0fd82265
Mohakad <mohkadtrd@gmail.com>
2024-04-26 21:38:30 -0700

    l

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-  
+
     url = "https://jsonplaceholder.typicode.com/"
     employee_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(employee_id)).json()
