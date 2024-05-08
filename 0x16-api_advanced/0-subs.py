e80da98b9c224a049e284791bd0f4d8e7b542ae5
Mohakad <mohkadtrd@gmail.com>
2024-05-08 15:28:41 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """n of sub"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return response.json().get('data', {}).get('subscribers', 0)
     else:
         return 0
