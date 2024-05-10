ec7fc262c4b6d9969021367c7e0d89d8a545555d
Mohakad <mohkadtrd@gmail.com>
2024-05-10 15:39:09 +0300

    0s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """n of sub"""
-    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/redditdev/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
